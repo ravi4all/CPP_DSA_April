440d3806ddcd1ea82443e3eef92255c8116778b8
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2022-09-04 14:59:26 +0530

    Add files via upload

--- 06-Backtracking/Sudoku.cpp
+++ 06-Backtracking/Sudoku.cpp
@@ -14,6 +14,41 @@ vector<vector<int>> board = {
         {0,0,0,4,1,9,0,0,5}, 
         {0,0,0,0,8,0,0,7,9}};
 
+bool isPresentInRow(int row, int data) {
+    for(int col = 0; col < MAX_SIZE; col++) {
+        if(board[row][col] == data) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isPresentInCol(int col, int data) {
+    for(int row = 0; row < MAX_SIZE; row++) {
+        if(board[row][col] == data) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isPresentInGrid(int row, int col, int data) {
+    int r = row - row % 3;
+    int c = col - col % 3;
+    for(int i = r; i < r + 3; i++) {
+        for(int j = c; j < c + 3; j++) {
+            if(board[i][j] == data) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool isSafe(int row, int col, int data) {
+    return !isPresentInRow(row, data) && !isPresentInCol(col, data) && !isPresentInGrid(row, col, data);
+}
+
 bool solveIt(int row, int col) {
 
     if(col == MAX_SIZE) {
@@ -43,30 +78,6 @@ bool solveIt(int row, int col) {
     return false;
 }
 
-bool isPresentInRow(int row, int data) {
-    for(int col = 0; col < MAX_SIZE; col++) {
-        if(board[row][col] == data) {
-            return true;
-        }
-    }
-}
-
-bool isPresentInCol(int col, int data) {
-    for(int row = 0; row < MAX_SIZE; row++) {
-        if(board[row][col] == data) {
-            return true;
-        }
-    }
-}
-
-bool isPresentInGrid(int row, int col, int data) {
-    
-}
-
-bool isSafe(int row, int col, int data) {
-    return !isPresentInRow(row, data) && !isPresentInCol(col, data) && !isPresentInGrid(row, col, data);
-}
-
 int main() {
-    solveIt(0,0);
+    cout << solveIt(0,0);
 }
