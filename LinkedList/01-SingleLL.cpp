819c36c1525e27090216debfbef0209d8e6da5fa
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2022-09-24 17:51:38 +0530

    Add files via upload

--- LinkedList/01-SingleLL.cpp
+++ LinkedList/01-SingleLL.cpp
@@ -13,8 +13,75 @@ void printList(Node *head) {
         cout << head->data << ",";
         head = head->next;
     }
+}
+
+void insertAtBeg(struct Node* head_ref,int data) {
+    Node* newNode = new Node;
+    newNode -> data = data;
+    newNode -> next = head_ref;
+    head_ref = newNode;
+}
+
+void insertAfterNode(struct Node* prev_node, int data) {
+    Node* newNode = new Node;
+    newNode -> data = data;
+    newNode -> next = prev_node->next;
+    prev_node->next = newNode;
+}
+
+void insertBeforeNode(struct Node** head_ref, struct Node* prev_node, int data) {
+    Node* newNode = new Node;
+    newNode -> data = data;
+    struct Node *temp = *head_ref;
+    while(temp->next->data != prev_node->data) {
+        temp = temp->next;
+    }
+    
+    newNode -> next = prev_node;
+    temp->next = newNode;
+}
+
+Node *insertAtEnd(struct Node* head_ref,int data) {
+    Node* newNode = new Node;
+    newNode -> data = data;
+    struct Node *temp = head_ref;
+
+    if(head_ref == NULL) {
+        head_ref = newNode;
+        return;
+    }
+
+    while(temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void insertAtAnyPos(int pos, int data) {
     
 }
+
+void deleteFromBeg(struct Node* head_ref) {
+    struct Node *temp = head_ref->next;
+    head_ref = temp;
+    free(temp);
+}
+
+void deleteFromEnd() {
+
+}
+
+void deleteFromAnyPos(struct Node* head_ref, int pos) {
+    int i = 1;
+    struct Node *temp = head_ref;
+    while(i < pos) {
+        temp = temp->next;
+        i++;
+    }
+    temp->next = temp->next->next;
+}
+
+
 
 int main() {
     struct Node* head = NULL;
@@ -34,6 +101,8 @@ int main() {
     n2->data = 30;
     n2->next = NULL;
 
+    printList(head);
+    insertAtBeg(head, 90);
     printList(head);
 
 }
