c403a427f1b682a0be09a3515969be6c4fe654f9
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2022-10-01 13:53:52 +0530

    Add files via upload

--- LinkedList/01-SingleLL.cpp
+++ LinkedList/01-SingleLL.cpp
@@ -140,6 +140,18 @@ Node *reverseRec(struct Node *currentNode, struct Node *prevNode) {
 
 }
 
+bool detectLoop(Node* head) {
+    Node *slow_p = head, *fast_p = head;
+    while(slow_p && fast_p && fast_p -> next) {
+        slow_p = slow_p->next;
+        fast_p = fast_p->next->next;
+        if(slow_p == fast_p) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
     struct Node* head = NULL;
     struct Node* n1 = NULL;
